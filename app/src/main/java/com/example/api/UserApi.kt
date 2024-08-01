49270e8debbd3783dcac21dc9eb13e0762e99468
Shivangee Rajput <100294737+ShivangeeRajput@users.noreply.github.com>
2024-08-01 14:02:07 +0530

    Update UserApi.kt

--- app/src/main/java/com/example/api/UserApi.kt
+++ app/src/main/java/com/example/api/UserApi.kt
@@ -4,11 +4,11 @@ import com.example.models.User
 import com.google.android.gms.common.api.Response
 
 interface ApiService {
-    @POST("user/register")
-    suspend fun registerUser(@Body user: User): Response<User>
+    // @POST("user/register")
+    // suspend fun registerUser(@Body user: User): Response<User>
 
-    @POST("user/login")
-    suspend fun loginUser(@Body loginRequest: LoginRequest): Response<User>
+    // @POST("user/login")
+    // suspend fun loginUser(@Body loginRequest: LoginRequest): Response<User>
 
 
 }
